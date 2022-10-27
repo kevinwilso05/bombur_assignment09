42ed1dd9a2462108015502f0a64eb1fb2066a411
Kevin Wilson <kevinw@interlink.com>
2022-10-26 20:20:25 -0400

    Added getter methods, changed name setter, added add player, get players and remove player methods to team, added players property to team.

--- bombur_assignment09/TeamPackage/TeamClass.py
+++ bombur_assignment09/TeamPackage/TeamClass.py
@@ -7,34 +7,70 @@ Semester/Year: Fall 2022
 Brief Description: Collaborating using Git and OOP constructs
 '''
 from pip._internal import req
+from PlayerPackage.PlayerClass import Player
 
 class Team(): # Written by Mark Johnson
-    def teamName(self, name):
+    def __init__(self, city, name, wins, players=None):
+        self.checkCity(city)
         self.checkName(name)
+        self.checkWins(wins)
+        self.Players = list(players)
+    def __repr__(self):
+        return self.city + " " + self.name + ", " + str(self.wins)
+    def __str__(self):
+        return "The " + self.city + " " + self.name + " have " + str(self.wins) + " wins"
+    
+    #Team Name Methods
+    def setTeamName(self, name):
+        self.checkName(name)
+    
     def checkName(self, name):
         if name == "Bengals":
             print ("Bengals is not a MLB team name.")
         else:
             self.name = name
-    def teamCity(self, city):
+            
+    def getTeamName(self):
+        return self.name
+    
+    # Team City Methods
+    def getTeamCity(self):
+        return self.city
+    
+    def setTeamCity(self, city):
         self.checkCity(city)
+        
     def checkCity(self, city):
         if city == "Columbus":
             print ("This city does not have a MLB team.")
         else:
             self.city = city
-    def teamWins(self, wins):
+
+    # Team Wins Methods        
+    def getTeamWins(self):        
+        return self.wins
+    
+    def setTeamWins(self, wins):
         self.checkWins(wins)
+        
     def checkWins(self, wins):
         if wins < 0:
             print ("Wins cannot be less than zero. Not even the Reds are that bad.")
         else:
             self.wins = wins
-    def __init__(self, city, name, wins):
-        self.checkCity(city)
-        self.checkName(name)
-        self.checkWins(wins)
-    def __repr__(self):
-        return self.city + " " + self.name + ", " + str(self.wins)
-    def __str__(self):
-        return self.city + " " + self.name + ", " + str(self.wins) + " wins"
+    def addTeamWin(self, num):
+        self.wins += num
+        
+    # Team Players Methods         
+    def addPlayer(self, player):
+        self.Players.append(player)
+        
+    def getPlayers(self):
+        [player.getPlayer() for player in self.Players]
+    # invokes the __repr__ dunder method in the player class
+    
+    def removePlayer(self, player):
+        self.Players.remove(player)
+        
+        
+    
